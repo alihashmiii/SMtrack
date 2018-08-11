971a21838e41fbe8d8c87a12021146429b3782e4
alihashmiii <alihashmi_87@hotmail.com>
2018-08-11 02:04:37 +0200

    update

--- SMTrack.m
+++ SMTrack.m
@@ -33,10 +33,10 @@ meanParticleDist[centroids_]:= Module[{vertices,dist},
 (* where distDelaunay is the meanParticleDist@labeledMat *)
 maxJumpDistance[centPrev_,centNew_,distDelaunay_]:= Module[{nearestFunc,rec,pts,\[ScriptCapitalA]},
  nearestFunc = Nearest@centPrev;
- rec = Flatten[Table[{i, Length@nearestFunc[#,{All, i}]},{i, 0, distDelaunay, 1}]&/@centNew, 1];
+ rec = Flatten[Table[{i, Length@nearestFunc[#,{All, i}]},{i, 1, distDelaunay, 1}]&/@centNew, 1];
  pts = Cases[rec, {_,_?(# <= 1 &)}];
- \[ScriptCapitalA] = WeightedData[Keys@#,Exp[Values[#]/Total[Values@#]]]&@Counts[Part[pts,All,1]];
- (*\[ScriptCapitalA] = WeightedData[Part[pts,All,1]]*)
+ (* \[ScriptCapitalA] = WeightedData[Keys@#,Exp[Values[#]/Total[Values@#]]]&@Counts[Part[pts,All,1]]; *)
+ \[ScriptCapitalA] = WeightedData[Part[pts,All,1]];
  N@*Mean@\[ScriptCapitalA]
 ];
 
@@ -91,45 +91,49 @@ Mean/@Transpose@box + {mx,my} - (Dimensions@data)/2.0
 *)
 
 modelFit[image_, mask_, shape_, box_] := Block[{pixelpos,pixelval,img,data,a,b,weights,data3D,
- mx,my,sx,sy,x,y,fm,dx,dy,cent,background,bestfit,intensityGuess,brightest},
+ mx,my,sx,sy,x,y,fm,dx,dy,cent,background,bestfit,intensityGuess,brightest,
+ dimMask = Dimensions@mask},
  pixelpos = mask["NonzeroPositions"];
  pixelval = PixelValue[image, pixelpos];
  brightest = Max@pixelval;
  img = ReplacePixelValue[shape, Thread[PixelValuePositions[shape, 1] -> pixelval]];
- data = ImageData@ImagePad[img, 2];
+ data = ImageData@ImagePad[img, 1];
  data3D = Flatten[MapIndexed[{First@#2, Last@#2, #1} &, data, {2}], 1];
  {dx,dy}= Dimensions@data;
  cent = N[(dx + dy)/2];
  intensityGuess = Max@data;
  fm = NonlinearModelFit[data3D,
   background+(a * Exp[-((x-mx)^2/(2*sx^2))-((y-my)^2/(2*sy^2))]),
- {{background,0.1},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
+ {{background,Automatic},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
  {x, y}];
  bestfit = fm["BestFitParameters"];
  (* if any param is less than 0 then we run a constrained fit *)
  If[Length[Position[bestfit[[All,2]],x_/;x<0]]>0,
   fm = NonlinearModelFit[data3D,
  {background + (a * Exp[-((x-mx)^2/(2*sx^2)) - ((y-my)^2/(2*sy^2))]),
- {background>0,a >0, mx >0, my > 0, sx >0, sy >0}},
- {{background,0.1},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
+ {background > 0, a > 0, mx > 0, my > 0, sx > 0, sy > 0}},
+ {{background,Automatic},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
  {x,y}
  ];
 ];
  bestfit = fm["BestFitParameters"];
  If[brightest>0.9,
  weights =  data3D[[All,3]];
- weights = weights/.{x_/;x<0.90 -> 1.0,x_/;x<1.0-> 0.0};
+ weights = weights/.{x_/; x<0.90 -> 1.0,x_/;x<1.0 -> 0.0};
  (* set all intensities > 0.90 to zero weights *)
  fm = NonlinearModelFit[data3D,
  {background + (a*Exp[-((x-mx)^2/(2*sx^2)) - ((y-my)^2/(2*sy^2))]),
- {background>0,a >0 ,mx>0, my>0, sx>0, sy>0}},
- {{background,0.1},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
+ {background > 0, a > 0 ,mx > 0, my > 0, sx > 0, sy > 0}},
+ {{background,Automatic},{a,intensityGuess},{mx,cent/2},{my,cent/2},{sx,cent/4},{sy,cent/4}},
  {x,y}, Weights->weights
  ];
 ];
  bestfit = fm["BestFitParameters"];
  {background,a,mx,my,sx,sy} = {background,a,mx,my,sx,sy} /. fm["BestFitParameters"];
- {(Mean/@Transpose@box + (-{mx,my} +{dx,dy}/2.0)),background,a,mx,my,sx,sy}
+ (* {(Mean/@Transpose@box + ({mx,my} -{dx,dy}/2.0)),background,a,mx,my,sx,sy} *)
+ If[AnyTrue[#,(#<0||#>First@dimMask||#>Last@dimMask)&],
+{Values@ComponentMeasurements[mask,"Centroid"],0,0,0,0,0,0}~FlattenAt~1,
+{#,background,a,mx,my,sx,sy}]&[(Mean/@Transpose@box+({mx,my}-{dx,dy}/2.0))]
 ];
 
 
